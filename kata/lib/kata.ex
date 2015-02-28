72d29cb8bd0b553663190b016fb4f315b051c979
steve b <meadsteve@gmail.com>
2015-02-28 09:34:05 +0000

    Simple solution for 1 to 8

--- kata/lib/kata.ex
+++ kata/lib/kata.ex
@@ -1,2 +1,21 @@
 defmodule Kata do
+
+  def to_numerals(number \\ 0, current_numeral \\ "")
+
+  def to_numerals(0, current_numeral) do
+    current_numeral
+  end
+
+  def to_numerals(number, current_numeral) when number >= 5 do
+    to_numerals(number - 5, current_numeral <> "V")
+  end
+
+  def to_numerals(number, current_numeral) when number >= 4 do
+    to_numerals(number - 4, current_numeral <> "IV")
+  end
+
+  def to_numerals(number, current_numeral) when number >= 1 do
+    to_numerals(number - 1, current_numeral <> "I")
+  end
+
 end
--- kata/test/kata_test.exs
+++ kata/test/kata_test.exs
@@ -1,7 +1,28 @@
 defmodule KataTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Zero results in an empty string" do
+    assert Kata.to_numerals(0) == ""
   end
+
+  test "1 to 3 are represented by Is" do
+    assert Kata.to_numerals(1) == "I"
+    assert Kata.to_numerals(2) == "II"
+    assert Kata.to_numerals(3) == "III"
+  end
+
+  test "4 is represented by IV" do
+    assert Kata.to_numerals(4) == "IV"
+  end
+
+  test "5 is represented by V" do
+    assert Kata.to_numerals(5) == "V"
+  end
+
+  test "6 to 8 are represented correctly" do
+    assert Kata.to_numerals(6) == "VI"
+    assert Kata.to_numerals(7) == "VII"
+    assert Kata.to_numerals(8) == "VIII"
+  end
+
 end
