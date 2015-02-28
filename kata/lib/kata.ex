06009678ccf6e1d26634498dbf912d5d909b1358
steve b <meadsteve@gmail.com>
2015-02-28 15:54:30 +0000

    string calc kata

--- kata/lib/kata.ex
+++ kata/lib/kata.ex
@@ -1,2 +1,28 @@
 defmodule Kata do
+
+  def string_add("") do
+    0
+  end
+
+  def string_add(input_string) do
+    input_string
+    |> split_the_string
+    |> convert_them_all_to_ints
+    |> Enum.sum
+  end
+
+  defp split_the_string(string) do
+    String.split(string, ~r/[, \n\t]/, trim: true)
+  end
+
+  defp convert_them_all_to_ints(string_numbers) do
+    Enum.map(string_numbers, &string_to_integer/1)
+  end
+
+  defp string_to_integer(number_string) do
+    {number, _} = Integer.parse(number_string)
+    if number < 0, do: raise(ArgumentError, message: "Negative numbers aren't allowed")
+    number
+  end
+
 end
--- kata/test/kata_test.exs
+++ kata/test/kata_test.exs
@@ -1,7 +1,41 @@
 defmodule KataTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "empty string returns 0" do
+    assert Kata.string_add("") == 0
   end
+
+  test "returns the value of a single number given to it" do
+    assert Kata.string_add("4") == 4
+    assert Kata.string_add("7") == 7
+  end
+
+  test "sums together two numbers seperated by commas" do
+    assert Kata.string_add("4,5") == 9
+    assert Kata.string_add("8,12") == 20
+  end
+
+  test "sums together three numbers seperated by commas" do
+    assert Kata.string_add("4,5,1") == 10
+    assert Kata.string_add("2,12,2") == 16
+  end
+
+  test "handles newlines and white space as well as commas" do
+    assert Kata.string_add("4 5") == 9
+    assert Kata.string_add("4\n5") == 9
+    assert Kata.string_add("4\t5") == 9
+  end
+
+  test "can trim out whitespace around numbers" do
+    assert Kata.string_add("4, 5") == 9
+    assert Kata.string_add("4 ,5") == 9
+    assert Kata.string_add("4, 5 ") == 9
+  end
+
+  test "fail when negative numbers are given" do
+    assert_raise ArgumentError, "Negative numbers aren't allowed", fn ->
+      Kata.string_add("4,-5")
+    end
+  end
+
 end
